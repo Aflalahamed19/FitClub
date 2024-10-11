7ea963e8e575d25775fdb077fd7867fe4a6b616c
Aflalahamed19 <mansooraflalahamed@gmail.com>
2024-10-11 11:31:33 +0530

    web site are compleated we need only mobile responsive cheack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
+import Join from './components/Join/Join';
+import Plans from './components/Programs/Plans/Plans';
+import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
+import Testimonials from './data/Testimonials/Testimonials';
 
 
 function App() {
   return (
     <div className="App">  
         <Hero/>
-
+        <Programs/>
+        <Reasons/>
+        <Plans/>
+        <Testimonials/>
+        <Join/>
+        <Footer/>
     </div>
   );
 }
